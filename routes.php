d2299c874c2f039e442d260540e373bd493e9dd4
Felice <felice.ostuni@uania.com>
2022-11-30 10:17:47 +0100

    fix

--- routes.php
+++ routes.php
@@ -7,7 +7,13 @@ use App\Modules\Knowledgebase\Http\Controllers\KnowledgeBaseTagController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('kb', [KnowledgeBaseController::class,'index'])->name('kbp.kb');
+Route::get('kb', [KnowledgeBaseController::class,'index'])
+    ->middleware(['web'])
+    ->name('kbp.kb')
+    ->crumbs(function ($crumbs) {
+            $crumbs->push('KnowledgeBase', route('kbp.kb'));
+    });
+
 Route::get('kb/categories/check_slug', [KnowledgeBaseCategoryController::class,'check_slug'])->name('kbp.categories.check_slug');
 Route::get('kb/categories/{slug}/{category}', [KnowledgeBaseCategoryController::class,'show'])->name('kbp.categories.show');
 Route::get('kb/articles/check_slug', [KnowledgeBaseArticleController::class,'check_slug'])->name('kbp.articles.check_slug');
